0dd0847092028c026a190520029865ec4bcb77a0
nowosad <tupiszakaczki@gmail.com>
2016-09-25 16:12:41 -0400

    dataset added

---
+++ R/pollen_count.R
@@ -0,0 +1,18 @@
+#' @name pollen_count
+#' @title Pollen count of alder, birch, and hazel
+#'
+#' @description pollen_count A dataset containing the synthetic data of alder, birch, and hazel pollen count in four locations ('Oz', 'Shire', 'Atlantis', 'Hundred Acre Wood') between 2007 and 2016
+#' 
+#' @format A data frame with 8352 rows and 5 variables:
+#' \itemize{
+#'     \item site
+#'     \item date
+#'     \item alder
+#'     \item birch
+#'     \item hazel
+#' }
+#' 
+#' @docType data
+#'
+
+NULL
