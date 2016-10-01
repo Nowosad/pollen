e5dcc7757ee426e0775546f63cb76eb919beb976
nowosad <tupiszakaczki@gmail.com>
2016-10-01 19:33:43 -0400

    send to cran version

---
+++ R/pollen.R
@@ -0,0 +1,9 @@
+#' pollen
+#'
+#' The pollen package offers a set of functions for working with aerobiological data.
+#' @docType package
+#' @name pollen
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1") utils::globalVariables(c("."))
