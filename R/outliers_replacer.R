bd238519b0062da1a308dd1dd968bd02f8125eae
nowosad <tupiszakaczki@gmail.com>
2016-10-13 21:54:51 -0400

    dplyr lag fixed

--- R/outliers_replacer.R
+++ R/outliers_replacer.R
@@ -10,8 +10,7 @@
 #' @return A new data.frame object with replaced outliers
 #' @importFrom lubridate year
 #' @importFrom purrr %>% map map_df map_dbl
-#' @importFrom dplyr lead
-#' @importFrom stats lag
+#' @importFrom dplyr lead lag
 #'  
 #' @references  Kasprzyk, I. and A. Walanus.: 2014. Gamma, Gaussian and Logistic Distribution Models for Airborne Pollen Grains and Fungal Spore Season Dynamics, Aerobiologia 30(4), 369-83.
 #' 
@@ -37,23 +36,20 @@ outliers_replacer <- function(x, value, date, threshold=5, ...){
                 map_df(rbind)
 }
 
-outliers_detector <- function(value, sum_percent=100, threshold){
-        df <- data.frame(value, value_m2=lag(value, 2), value_p2=lead(value, 2),
-                         value_m1=lag(value, 1), value_p1=lead(value, 1))
-        df$backgroud <- threshold * (((df$value_m2 + df$value_p2)/6) + ((df$value_m1 + df$value_p1)/3))
-        which(df$value>(sum(df$value)/sum_percent) & df$value>df$backgroud)
-}
-
-single_outlier_replacer <- function(indx, value, threshold){
-        threshold * (((value[indx-2] + value[indx+2])/6) + ((value[indx-1] + value[indx+1])/3))
-        
-}
-
 outliers_replacer_single_year <- function(x, value, threshold, ...){
         indx <- outliers_detector(value=x[[value]], threshold=threshold, ...)
         new_value <- indx %>% map_dbl(~(single_outlier_replacer(., value=x[[value]], threshold=threshold))) 
         x[[value]][indx] <- new_value
-        x
+        return(x)
 }
 
+single_outlier_replacer <- function(indx, value, threshold){
+        return(threshold * (((value[indx-2] + value[indx+2])/6) + ((value[indx-1] + value[indx+1])/3)))
+}
 
+outliers_detector <- function(value, sum_percent=100, threshold){
+        df <- data.frame(value, value_m2=lag(value, 2), value_p2=lead(value, 2),
+                         value_m1=lag(value, 1), value_p1=lead(value, 1))
+        df$backgroud <- threshold * (((df$value_m2 + df$value_p2)/6) + ((df$value_m1 + df$value_p1)/3))
+        return(which(df$value>(sum(df$value)/sum_percent) & df$value>df$backgroud))
+}
