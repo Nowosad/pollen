975e02f897ea7f0eed57a6c304e7229937e08a71
jn <tupiszakaczki@gmail.com>
2018-01-06 19:59:37 -0500

    gdd update (simplification of arguments nr)

--- R/gdd.R
+++ R/gdd.R
@@ -7,7 +7,6 @@
 #' @param tmin daily minimum temperature
 #' @param tbase base temperature
 #' @param tbase_max maximum base temperature
-#' @param x data.frame object containing the data
 #'
 #' @return a numeric vector with GDD values
 #'
@@ -19,11 +18,10 @@
 #' set.seed(25)
 #' df <- data.frame(tmax=runif(100, 6, 10), tmin=runif(100, 4,6))
 #'
-#' gdd(df, tmax='tmax', tmin='tmin', tbase=5, tbase_max=30)
+#' gdd(tmax = df$tmax, tmin = df$tmin, tbase = 5, tbase_max = 30)
 #'
 #'
-
-gdd <- function(x, tmax, tmin, tbase, tbase_max) {
+gdd <- function(tmax, tmin, tbase, tbase_max) {
   adjust_for_tbase <- function(x, tbase) {
     ifelse(test = x < tbase, yes = tbase, no = x)
   }
@@ -31,10 +29,8 @@ gdd <- function(x, tmax, tmin, tbase, tbase_max) {
     ifelse(test = x > tbase_max, yes = tbase_max, no = x)
   }
 
-  x <- as.data.frame(x)
-
-  tmax_adjusted <- adjust_for_tbase(x[, tmax], tbase)
-  tmin_adjusted <- adjust_for_tbase(x[, tmin], tbase)
+  tmax_adjusted <- adjust_for_tbase(tmax, tbase)
+  tmin_adjusted <- adjust_for_tbase(tmin, tbase)
 
   tmax_adjusted <- adjust_for_tbase_max(tmax_adjusted, tbase_max)
   tmin_adjusted <- adjust_for_tbase_max(tmin_adjusted, tbase_max)
