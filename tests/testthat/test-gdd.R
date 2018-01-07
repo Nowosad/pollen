bd762db75c4889c15b5c4ce6d8d3319e10dc8a91
jn <tupiszakaczki@gmail.com>
2018-01-06 20:03:30 -0500

    gdd function docs update

--- tests/testthat/test-gdd.R
+++ tests/testthat/test-gdd.R
@@ -3,6 +3,6 @@ expect_numeric <- function(x) expect_is(x, "numeric")
 
 test_that("gdd works", {
   df <- data.frame(tmax = runif(100, 6, 10), tmin = runif(100, 4, 6))
-  results <- gdd(df, tmax = "tmax", tmin = "tmin", tbase = 5, tbase_max = 30)
+  results <- gdd(df$tmax, df$tmin, tbase = 5, tbase_max = 30)
   expect_numeric(results)
 })
