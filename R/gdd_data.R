efe0c62646b05164aeed964f2e0194779bf56a5b
Nowosad <tupiszakaczki@gmail.com>
2020-02-12 16:39:35 +0100

    adds new dataset

---
+++ R/gdd_data.R
@@ -0,0 +1,16 @@
+#' @name gdd_data
+#' @title Exemplary dataset for GDD calculations
+#'
+#' @description gdd_data A dataset containing a synthetic data of day, tmax (daily maximum temperature), and tmin (daily minimum temperature)
+#'
+#' @format A data frame with 100 rows and 3 variables:
+#' \itemize{
+#'     \item day
+#'     \item tmax
+#'     \item tmin
+#' }
+#'
+#' @docType data
+#'
+
+NULL
